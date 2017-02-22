bf135f91caf6fbdadb458c73de7c5f1c9140b6ff
sean <searn@ualberta.ca>
2017-02-21 19:38:51 -0700

    lane lines using udacity example

--- advanced_lane_lines.py
+++ advanced_lane_lines.py
@@ -4,9 +4,9 @@ import numpy as np
 import cv2
 import os
 
-
-cv2.namedWindow( "Overall", cv2.WINDOW_AUTOSIZE );
 test_images = os.listdir("test_images/")
+undistorted_images = os.listdir("undistorted_images/")
+
 
 
 def calibrate_camera(nx,ny,file_path):
@@ -42,11 +42,18 @@ def calibrate_camera(nx,ny,file_path):
 
     return mtx, dist
 
-def binarize_image(img, sobel_kernel = 3, sx_thresh =(20,100),s_thresh=(170,255) , sdir_thresh = (0,np.pi/2)):
+def binarize_image(img, sobel_kernel = 3, sx_thresh =(20,100),sy_thresh =(25,255),s_thresh=(170,255) , sdir_thresh = (0,np.pi/2), hue_thresh = (20,50),lightness_thresh = (150,255)):
+    
+    cv2.imshow("raw",img)
     hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
+    #hls = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-    gray = hls[:,:,2]
-
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    saturation = hls[:,:,2]
+    hue = hls[:,:,0]
+    lightness = hls[:,:,1]
+    
+    
     sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0,sobel_kernel) 
     sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1,sobel_kernel)
 
@@ -60,31 +67,134 @@ def binarize_image(img, sobel_kernel = 3, sx_thresh =(20,100),s_thresh=(170,255)
     scaled_sobel = np.uint8(255*abs_sobelx/np.max(abs_sobelx))
     sxbinary = np.zeros_like(sdir_binary) 
     sxbinary[(scaled_sobel >= sx_thresh[0]) & (scaled_sobel <= sx_thresh[1])] = 1 
+    
+    scaled_sobel = np.uint8(255*abs_sobely/np.max(abs_sobely))
+    sybinary = np.zeros_like(sdir_binary) 
+    sybinary[(scaled_sobel >= sy_thresh[0]) & (scaled_sobel <= sy_thresh[1])] = 1 
+
+    #kernel = np.ones((5,5),np.uint8)
+    #sybinary = cv2.dilate(sybinary,kernel,iterations = 1)
+
 
     s_binary = np.zeros_like(sdir_binary) 
-    s_binary[(gray >= s_thresh[0]) & (gray <= s_thresh[1])] = 1
+    s_binary[(saturation >= s_thresh[0]) & (saturation <= s_thresh[1])] = 1
+    #kernel = np.ones((5,5),np.uint8)
+    #s_binary = cv2.dilate(s_binary,kernel,iterations = 1)
+
+
+    hue_binary = np.zeros_like(sdir_binary)
+    hue_binary[ (hue >= hue_thresh[0]) & (hue <= hue_thresh[1])] = 1
+
+    lightness_binary = np.zeros_like(sdir_binary)
+    lightness_binary[ (lightness >= lightness_thresh[0]) & (lightness <= lightness_thresh[1])] = 1
 
     color_binary = np.dstack(( np.zeros_like(sxbinary), sxbinary, s_binary)) 
     #color_binary = np.dstack(( sdir_binary, sxbinary, s_binary)) 
     overall_binary = np.zeros_like(sdir_binary)
     #overall_binary[(s_binary==1) & (sdir_binary==1) & (sxbinary==1)] = 1
-    overall_binary[(s_binary==1) | (sxbinary==1)] = 1
+    overall_binary[((sybinary==1) & (sxbinary==1)) | (((s_binary==1) & (lightness_binary==1))==1)] = 1
+
+
+
+    # try dir gradient again!!!!!!!!!!!!
 
 
     #cv2.imshow("color_binary", color_binary)
     #cv2.imshow("Overall", overall_binary)
     #cv2.imshow("sxbinary",sxbinary)
     #cv2.imshow("s_binary", s_binary)
+    #cv2.imshow("hue_binary", hue_binary)
+    #cv2.imshow("light_binary", lightness_binary)
+    #cv2.imshow("sy_binary", sybinary)
     #cv2.imshow("sdir_binary", sdir_binary)
     #plt.imshow(s_binary)
     #plt.waitforbuttonpress()
     #cv2.waitKey(0)
     return overall_binary
 
+def find_lane_lines(img):
+    histogram = np.sum(img[img.shape[0]/2:,:], axis=0)
+    out_img = np.dstack((img, img, img))*255
+
+    midpoint = np.int(histogram.shape[0]/2)
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(img.shape[0]/nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = img.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = img.shape[0] - (window+1)*window_height
+        win_y_high = img.shape[0] - window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
+        cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:        
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds] 
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds] 
+
+    # Fit a second order polynomial to each
+    left_fit = np.polyfit(lefty, leftx, 2)
+    right_fit = np.polyfit(righty, rightx, 2)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, img.shape[0]-1, img.shape[0] )
+    left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+    right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+    plt.imshow(out_img)
+    plt.plot(left_fitx, ploty, color='yellow')
+    plt.plot(right_fitx, ploty, color='yellow')
+    plt.xlim(0, 1280)
+    plt.ylim(720, 0)
+    plt.waitforbuttonpress()
+    plt.close()
 
 
 if __name__ == "__main__":
-    camera_mtx, distortion = calibrate_camera(9,6,"camera_cal/")
+    #camera_mtx, distortion = calibrate_camera(9,6,"camera_cal/")
 
     #img = mpimg.imread("camera_cal/calibration1.jpg")
     #cv2.imshow("raw",img)
@@ -95,9 +205,13 @@ if __name__ == "__main__":
     
 
     for i,x in enumerate(test_images):
-        img = cv2.imread("test_images/" + x)
+        #img = cv2.imread("test_images/" + x)
         #cv2.imshow("Raw",img)
-        undist = cv2.undistort(img,camera_mtx,distortion,camera_mtx, None)
+        #undist = cv2.undistort(img,camera_mtx,distortion,camera_mtx, None)
+        #cv2.imwrite("undistorted_images/" + x,undist)
+        print(x)
+        undist = cv2.imread("undistorted_images/"+ x)
+
         #cv2.imshow("Undist",undist)
 
         hls = cv2.cvtColor(undist, cv2.COLOR_BGR2HLS)
@@ -111,7 +225,7 @@ if __name__ == "__main__":
         #cv2.imshow("V",hsv[:,:,2])
         #gray = cv2.cvtColor(undist, cv2.COLOR_BGR2GRAY)
         #cv2.imshow("GRAY",S)
-        binary = binarize_image(undist,sobel_kernel = 3, sx_thresh =(20,100),s_thresh=(170,255),sdir_thresh = (0.95,1.0))
+        binary = binarize_image(undist,sobel_kernel = 3, sx_thresh =(10,255),lightness_thresh = (100,255), sy_thresh=(25,255),s_thresh=(130,255))
         #plt.imshow(binary)
         #plt.waitforbuttonpress()
         #TL, TR, BL, BR
@@ -126,9 +240,11 @@ if __name__ == "__main__":
         
         # polygon [(224,634), (489,484), (1026,634), (802,484)]
         # corrected []
-        print(warped.shape)
-        cv2.imshow("top down",warped)
-        cv2.waitKey(0)
+        find_lane_lines(warped)
+
+        
+        #cv2.imshow("top down",warped)
+        #cv2.waitKey(0)
         
 
 
