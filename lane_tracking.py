26107926fa15355c8603eb4385448382b058d6f6
sean <searn@ualberta.ca>
2017-02-24 22:05:04 -0700

    keeping line info

--- lane_tracking.py
+++ lane_tracking.py
@@ -33,6 +33,75 @@ class Line():
         #y values for detected line pixels
         self.ally = None
 
+def get_line(linex_base,img,margin, minpix,old_line):
+
+    out_img = np.dstack((img, img, img))*255
+    line = old_line
+    
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(img.shape[0]/nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = img.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+
+    x_current = linex_base
+
+    lane_inds = []
+
+    countGoodWindows = 0
+    # Step through the windows one by one
+    for window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = img.shape[0] - (window+1)*window_height
+        win_y_high = img.shape[0] - window*window_height
+        win_x_low = x_current - margin
+        win_x_high = x_current + margin
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img,(win_x_low,win_y_low),(win_x_high,win_y_high),(0,255,0), 2) 
+        #cv2.imshow("out_img",out_img)
+        # Identify the nonzero pixels in x and y within the window
+        good_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_x_low) & (nonzerox < win_x_high)).nonzero()[0]
+        # Append these indices to the lists
+        lane_inds.append(good_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_inds) > minpix:
+            x_current = np.int(np.mean(nonzerox[good_inds]))
+            countGoodWindows += 1
+    # Concatenate the arrays of indices
+    lane_inds = np.concatenate(lane_inds)
+    
+    linex = nonzerox[lane_inds]
+    liney = nonzeroy[lane_inds] 
+    
+    # Fit a second order polynomial to each
+    line_fit = np.polyfit(liney, linex, 2)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, img.shape[0]-1, img.shape[0] )
+    fitx = line_fit[0]*ploty**2 + line_fit[1]*ploty + line_fit[2]
+
+    if(len(line.recent_xfitted) > line.n):
+        line.recent_xfitted.pop(0)
+
+    
+    line.diffs = np.subtract(line_fit,line.current_fit)
+    line.recent_xfitted.append(fitx)
+    line.bestx = np.mean(line.recent_xfitted,axis=0)
+    line.best_fit = np.polyfit(liney, line.bestx, 2)
+    line.current_fit = line_fit
+    line.radius_of_curvature = get_radius_curvature(best_fit)
+    line.line_base_pos = abs(img.shape[1] / 2 - linex_base) * xm_per_pix 
+    line.allx = linex
+    line.ally = liney
+
+
+    out_img[nonzeroy[lane_inds], nonzerox[lane_inds]] = [255, 0, 0]
+    
+    return line_fit
+
 def find_lane_lines(img):
     right_line = Line()
     left_line = Line()
@@ -210,6 +279,67 @@ def update_line_with_new_frame(img,left_fit, right_fit):
     
 
     return left_fit, right_fit
+
+def update_line(img,line, linex_base):
+
+    if(sanity_check(img) is False):
+        return False, None, None
+
+    line_fit = line.current_fit
+
+    nonzero = img.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    margin = 100
+    line_lane_inds = ((nonzerox > (line_fit[0]*(nonzeroy**2) + line_fit[1]*nonzeroy + line_fit[2] - margin)) & (nonzerox < (line_fit[0]*(nonzeroy**2) + line_fit[1]*nonzeroy + line_fit[2] + margin))) 
+
+    # Again, extract left and right line pixel positions
+    linex = nonzerox[line_lane_inds]
+    liney = nonzeroy[line_lane_inds] 
+    # Fit a second order polynomial to each
+    line_fit = np.polyfit(liney, linex, 2)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, img.shape[0]-1, img.shape[0] )
+    line_fitx = line_fit[0]*ploty**2 + line_fit[1]*ploty + line_fit[2]
+
+    # Create an image to draw on and an image to show the selection window
+    out_img = np.dstack((img, img, img))*255
+    window_img = np.zeros_like(out_img)
+    # Color in left and right line pixels
+    out_img[nonzeroy[line_lane_inds], nonzerox[line_lane_inds]] = [255, 0, 0]
+
+    # Generate a polygon to illustrate the search window area
+    # And recast the x and y points into usable format for cv2.fillPoly()
+    line_line_window1 = np.array([np.transpose(np.vstack([line_fitx-margin, ploty]))])
+    line_line_window2 = np.array([np.flipud(np.transpose(np.vstack([line_fitx+margin, ploty])))])
+    line_line_pts = np.hstack((line_line_window1, line_line_window2))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(window_img, np.int_([line_line_pts]), (0,255, 0))
+    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+    #plt.imshow(result)
+    #plt.plot(left_fitx, ploty, color='yellow')
+    #plt.plot(right_fitx, ploty, color='yellow')
+    #plt.xlim(0, 1280)
+    #plt.ylim(720, 0)
+
+    #plt.waitforbuttonpress()
+    #plt.close()
+    if(len(line.recent_xfitted) > line.n):
+        line.recent_xfitted.pop(0)
+
+    line.diffs = np.subtract(line_fit,line.current_fit)
+    line.recent_xfitted.append(line_fitx)
+    line.bestx = np.mean(line.recent_xfitted,axis=0)
+    line.best_fit = np.polyfit(liney, line.bestx, 2)
+    line.current_fit = line_fit
+    line.radius_of_curvature = get_radius_curvature(best_fit)
+    line.line_base_pos = abs(img.shape[1] / 2 - linex_base) * xm_per_pix 
+    line.allx = linex
+    line.ally = liney
+
+    return line
 
 def get_radius_curvature(line_fit):
     ploty = np.linspace(0, 719, num=720)
